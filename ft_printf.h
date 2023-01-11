477189f164b667f9e43056af730fe97dd93c8fc2
alnavarr <109280975+itsb3la@users.noreply.github.com>
2023-01-11 09:51:23 +0100

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -6,19 +6,26 @@
 /*   By: alnavarr <alnavarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 12:04:48 by alnavarr          #+#    #+#             */
-/*   Updated: 2022/12/21 12:08:48 by alnavarr         ###   ########.fr       */
+/*   Updated: 2022/12/27 11:51:39 by alnavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-# include <unistd.h>
-# include <stdlib.h>
+# include<stdarg.h>
+# include<stdlib.h>
+# include<unistd.h>
+# include<stdio.h>
 
-int	ft_type(va_list	arg, char const type, int count);
+int	ft_printf(char const *s, ...);
 int	ft_printchar(int c, int count);
 int	ft_printstr(char *s, int count);
-
-
+int	ft_type(va_list arg, char const type, int count);
+int	ft_printint(int num, int count);
+int	ft_printnum(unsigned int num, int count);
+int	ft_printpt(void *pt, int count);
+int	ft_convert(unsigned long int num, int count);
+int	ft_printhexamin(unsigned int num, int count);
+int	ft_printhexamax(unsigned int num, int count);
 #endif
