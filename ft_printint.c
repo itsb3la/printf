fbbf5ca40e02fa4fa14734ecc3bd25216b2219b3
alnavarr <109280975+itsb3la@users.noreply.github.com>
2023-09-20 19:35:23 +0200

    Add files via upload

--- ft_printint.c
+++ ft_printint.c
@@ -3,11 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printint.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alnavarr <alnavarr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alnavarr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/05 12:26:40 by alnavarr          #+#    #+#             */
-/*   Updated: 2022/12/05 13:46:30 by alnavarr         ###   ########.fr       */
+/*   Created: 2023/09/19 16:44:25 by alnavarr          #+#    #+#             */
+/*   Updated: 2023/09/20 19:21:50 by alnavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include"ft_printf.h"
+
+int	ft_printint(int num, int count)
+{
+	if (num == -2147483648)
+	{
+		count = ft_printstr("-2147483648", count);
+		if (count == -1)
+			return (-1);
+	}
+	else
+	{
+		if (num < 0)
+		{
+			count = ft_printchar('-', count);
+			if (count == -1)
+				return (-1);
+			num *= -1;
+		}
+		count = ft_printnum(num, count);
+		if (count == -1)
+			return (-1);
+	}
+	return (count);
+}
+
+int	ft_printnum(unsigned int num, int count)
+{
+	if (num >= 10)
+	{
+		count = ft_printnum(num / 10, count);
+		if (count == -1)
+			return (-1);
+	}
+	count = ft_printchar((num % 10) + '0', count);
+	if (count == -1)
+		return (-1);
+	return (count);
+}
--- ft_printpt.c
+++ ft_printpt.c
@@ -3,11 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printpt.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alnavarr <alnavarr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alnavarr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/05 12:26:45 by alnavarr          #+#    #+#             */
-/*   Updated: 2022/12/05 13:46:32 by alnavarr         ###   ########.fr       */
+/*   Created: 2023/09/19 16:45:03 by alnavarr          #+#    #+#             */
+/*   Updated: 2023/09/20 19:22:21 by alnavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	ft_printpt(void *pt, int count)
+{
+	count = ft_printstr("0x", count);
+	if (count == -1)
+		return (-1);
+	count = ft_convert((unsigned long int) pt, count);
+	if (count == -1)
+		return (-1);
+	return (count);
+}
+
+int	ft_convert(unsigned long int num, int count)
+{
+	char	*base;
+
+	base = "0123456789abcdef";
+	if (num >= 16)
+	{
+		count = ft_convert(num / 16, count);
+		if (count == -1)
+			return (-1);
+	}
+	count = ft_printchar(base[num % 16], count);
+	if (count == -1)
+		return (-1);
+	return (count);
+}
